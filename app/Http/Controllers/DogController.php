c8a20d9a965b560f9787d81b7858b88986f18e24
Christian Powell <christiant.powell@gmail.com>
2018-10-25 10:06:23 -0500

    Create controller for index

--- app/Http/Controllers/DogController.php
+++ app/Http/Controllers/DogController.php
@@ -6,12 +6,8 @@ use Illuminate\Http\Request;
 
 class DogController extends Controller
 {
-    public  function index() {
-
-        return view('welcome');
-    }
-
-    public function info($name) {
+    public function info($name)
+    {
         return 'You are reading the food amount for' . $name;
     }
 }
